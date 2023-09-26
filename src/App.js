2796ccbcdd96412da677cb9e72c72bc87ea9b12c
Jose Jimenez <joseluisjimagui@gmail.com>
2023-09-26 01:53:56 -0600

    Import react router and create directory components with footer and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
-      AllForOne
+      <Header />
+      <h1>AllForOne</h1>
+      <Footer />
+
     </div>
   );
 }
